c75b700c9b01c5873a9eda43f445d17244c2e43e
claire ogesen <claire.ogesen.co@gmail.com>
2021-02-27 13:16:49 -0700

    1st

---
+++ assets/quiz.js
@@ -0,0 +1,28 @@
+//quiz hame that starts with button click, runs a timer 
+// has 5 quesitons, multipule choice, each answer moves to the net question 
+//incorrect aswers detract time 
+// shows high score 
+// use localstorage to save high scores 
+//boiler plate html link to bootstrap css jss 
+// eventListener, jquery. setTime
+//on.click 
+//display none Display: block
+
+// dynamic rendering 
+//array variable for questions and answers and correctness 
+
+// const question = [
+//     {q "what color is a fire truck",
+//     c: ["green", "blue", "red", "yello"],
+//     a: "red"
+// }, 
+// {
+//     q: "what colr is the sky",
+//     c: ["pink", "blue", "green"],
+//     a: "red"
+// }
+// ];
+
+$("#clickMe").on("click", function(){
+console.log("WORKS")
+});
