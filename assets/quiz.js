28bc74964ca3616451c206bf279f07fa0694ddef
claire ogesen <claire.ogesen.co@gmail.com>
2021-02-28 14:37:06 -0700

    questions showing up

--- assets/quiz.js
+++ assets/quiz.js
@@ -8,16 +8,66 @@
 //on.click 
 //display none Display: block
 
+
+var timeRemaining = 300000;
+var timeTicks = 1000;
+
+//set up 
+var questionIndex = 0;
+
+function updateTime() {
+    timeRemaining -= timeTicks;
+
+    var timerText = document.getElementById("countdown");
+    timerText.innerHTML = "Timer" + timeRemaining;
+
+    if(timeRemaining <= 0) {
+        window.clearInterval(timeTicks);
+        window.alert("time expired");
+    }
+}
+
 var startButton = document.getElementById('clickMe')
 startButton.addEventListener("click", StartQuiz);
 
-function StartQuiz() {
-    setNextQuestion()
+function StartQuiz(){
+    window.setInterval(updateTime, timeTicks);
+
+    //show element by id 
+
+    var quizArea = document.getElementById("quiz");
+    quizArea.style.display = "block";
+
+    displayNextQuestion();
 }
 
-function NextQuestion() {
+function displayNextQuestion() {
+    //set question text 
+    var questionText = document.getElementById("question");
+    questionText.innerHTML = question[questionIndex]["question"];
 
+    //clear existing answer 
+    var radioButtons = document.getElementsByName("answer");
+    for (var i=0; i <radioButtons.length; i++){
+        radioButtons[i].checked=false;
+    }
+//populate radio button 
+var answer1  =  document.getElementById("answer1");
+var answer2  =  document.getElementById("answer2");
+var answer3  =  document.getElementById("answer3");
+var answer4  =  document.getElementById("answer4");
+
+answer1.nextSibling.textContent = question[questionIndex]["answer"][0]["text"];
+answer2.nextSibling.textContent = question[questionIndex]["answer"][1]["text"];
+answer3.nextSibling.textContent = question[questionIndex]["answer"][2]["text"];
+answer4.nextSibling.textContent = question[questionIndex]["answer"][3]["text"];
+
+
+
+console.log("QUESTION IS: " + question['question'])
 }
+
+
 
 var question = [
     {
@@ -57,7 +107,7 @@ var question = [
         ]
     },
     {
-        question: "Which of these values is NOT considered false? "
+        question: "Which of these values is NOT considered false? ",
         answer: [
             { text: "0", correct: false },
             { text: "'0'", correct: true},
