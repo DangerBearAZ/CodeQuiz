7ea726c050bc8b06fb5f99a2d76fc9935e0c0dec
claire ogesen <claire.ogesen.co@gmail.com>
2021-02-28 12:51:44 -0700

    update

--- assets/quiz.js
+++ assets/quiz.js
@@ -8,21 +8,62 @@
 //on.click 
 //display none Display: block
 
-// dynamic rendering 
-//array variable for questions and answers and correctness 
+var startButton = document.getElementById('clickMe')
+startButton.addEventListener("click", StartQuiz);
 
-// const question = [
-//     {q "what color is a fire truck",
-//     c: ["green", "blue", "red", "yello"],
-//     a: "red"
-// }, 
-// {
-//     q: "what colr is the sky",
-//     c: ["pink", "blue", "green"],
-//     a: "red"
-// }
-// ];
+function StartQuiz() {
+    setNextQuestion()
+}
 
-$("#clickMe").on("click", function(){
-console.log("WORKS")
-});
+function NextQuestion() {
+
+}
+
+var question = [
+    {
+        question: "A property of an object that is a function is called a",
+        answer: [
+            { text: "method", correct: true},
+            { text: "string", correct: false},
+            { text: "stylesheet", correct: false},
+            { text: "child", correct:false}
+        ]
+    },
+    {
+        question: "What does DOM stand for", 
+        answer: [
+            { text: "Document Object Model", correct: true},
+            { text: "Desktop Oriented Mode", correct: false},
+            { text: "Digital Ordinance Model", correct: false},
+            { text: "Do Orangutans Moo", correct: false}
+        ]
+    },
+    {
+        question: "What HTML attribute references and external JavaScript Files?",
+        answer: [
+            { text: "index", correct: false},
+            { text: "src", correct: true},
+            { text: "href", correct: false},
+            { text: "id", correct: false}
+        ]
+    },
+    {
+        question: "Arrays in Javascript can be used to store?",
+        answer: [
+            { text: "numbers and strings", correct: false},
+            { text: "other arrays", correct: false},
+            { text: "Booleans", correct: false},
+            { text: "All of the above", correct: true}
+        ]
+    },
+    {
+        question: "Which of these values is NOT considered false? "
+        answer: [
+            { text: "0", correct: false },
+            { text: "'0'", correct: true},
+            { text:"null", correct:false},
+            { text:"''", correct:false}
+        ]
+    }
+
+]
