ece0bd08d3bfa3c5bae07b4f12e395d50214f553
claire ogesen <claire.ogesen.co@gmail.com>
2021-02-28 17:46:50 -0700

    update

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,12 +1,3 @@
-//quiz hame that starts with button click, runs a timer 
-// has 5 quesitons, multipule choice, each answer moves to the net question 
-//incorrect aswers detract time 
-// shows high score 
-// use localstorage to save high scores 
-//boiler plate html link to bootstrap css jss 
-// eventListener, jquery. setTime
-//on.click 
-//display none Display: block
 
 
 var timeRemaining = 300000;
